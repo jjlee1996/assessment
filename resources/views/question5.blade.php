07870be62c366e8d873728b3139e4d5bc6de279c
Lee Jiunn Jye <jjlee1996@gmail.com>
2025-05-25 16:39:39 +0800

    push

--- resources/views/question5.blade.php
+++ resources/views/question5.blade.php
@@ -13,7 +13,6 @@
             <li><label>Is this a good database design? Why?</label></li>
             <p>No, this db only support single item as each ORDERID only bind to one item and naming issue for column.
             </p>
-
             <li><label>If your answer to question 1 is no, what can you propose to improve the design?</label></li>
             <p>
                 UNIT PRICE need to rename to UNIT_PRICE. <br>
@@ -22,7 +21,6 @@
                 A new column USERID should insert into TBL_TRANSACTIONS and drop TBL_ORDERS because it just link USERID
                 and ORDERID.
             </p>
-
             <table>
                 <thead>
                     <tr>
@@ -91,6 +89,65 @@
                     </tr>
                 </tbody>
             </table>
+            <li><label>Write a SQL query based on the above schema to retrieve information about the following
+                    items. Assume that the TRANSACTION_DATE column is in the UTC time zone. We need to
+                    generate reports in Malaysia time.</label>
+                <ul>
+                    <li type="a">Who is the top spender in this online store?</li>
+                    <pre>
+                        <code>
+                        SELECT
+                            C.`USERID`,
+                            C.`NAME`,
+                            SUM(T.`QUANTITY` * T.`UNIT PRICE`) AS TOTAL_SPENT
+                        FROM
+                            tbl_customers AS C
+                        JOIN tbl_orders AS O
+                            ON C.`USERID` = O.`USERID`
+                        JOIN tbl_transactions AS T
+                            ON O.`ORDERID` = T.`ORDERID`
+                        GROUP BY
+                            C.`USERID`
+                        ORDER BY
+                            `TOTAL_SPENT` DESC
+                        LIMIT 1;
+                        </code>
+                    </pre>
+
+                    <li type="a">Tell me the number of transactions in each hour of the day.</li>
+                    <pre>
+                        <code>
+                        SELECT 
+                            HOUR(CONVERT_TZ(T.`TRANSACTION_DATE`, '+00:00', '+08:00')) AS HOUR,
+                            COUNT(*) AS NO_OF_TRANSACTION
+                        FROM 
+                            tbl_transactions AS T
+                        GROUP BY 
+                            HOUR
+                        ORDER BY 
+                            HOUR;
+                        </code>
+                    </pre>
+
+                    <li type="a">What kind of fruits (item) did Adam buy so far?</li>
+                    <pre>
+                        <code>
+                        SELECT DISTINCT 
+                            T.`ITEM`
+                        FROM 
+                            tbl_customers AS C
+                        JOIN 
+                            tbl_orders AS O ON C.`USERID` = O.`USERID`
+                        JOIN 
+                            tbl_transactions AS T ON O.`ORDERID` = T.`ORDERID`
+                        WHERE 
+                            C.`NAME` = 'Adam';
+                        </code>
+                    </pre>
+                </ul>
+
+            </li>
+
         </ol>
 
     </div>
